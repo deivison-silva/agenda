58c197fc667606613bc161817b63b17be97e119a
Deivison Silva <deivisondamiao01@outlook.com>
2022-03-20 16:39:35 -0300

    retrives data from database and show them on index

--- contatos/models.py
+++ contatos/models.py
@@ -8,12 +8,18 @@ from django.utils import timezone
 class Categoria(models.Model):
     nome = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.nome
+
 
 class Contato(models.Model):
     nome = models.CharField(max_length=100)
-    sobrenome = models.CharField(max_length=100)
+    sobrenome = models.CharField(max_length=100, blank=True)
     telefone = models.CharField(max_length=100)
     email = models.CharField(max_length=100, blank=True)
     data_criacao = models.DateTimeField(default=timezone.now)
-    descricao = models.TextField()
+    descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
--- contatos/views.py
+++ contatos/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from .models import Contato, Categoria
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'contatos/index.html')
+    contatos = Contato.objects.order_by('-data_criacao').all()
+
+    dados = {
+        'contatos': contatos
+    }
+
+    return render(request, 'contatos/index.html', dados)
