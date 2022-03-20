1af23537f78a6eb57f30fc876a403f4b8e522d2c
Deivison Silva <deivisondamiao01@outlook.com>
2022-03-20 18:04:51 -0300

    creates specific contato template, adds new unique ulr and also adds function contato

--- contatos/urls.py
+++ contatos/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.contatos, name='contatos'),
+    path('<int:id>', views.contato, name='contato')
 ]
--- contatos/views.py
+++ contatos/views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render
-from .models import Contato, Categoria
+from django.shortcuts import render, get_object_or_404
+from .models import Contato
 # Create your views here.
 
 
-def index(request):
+def contatos(request):
     contatos = Contato.objects.order_by('-data_criacao').all()
 
     dados = {
         'contatos': contatos
     }
 
-    return render(request, 'contatos/index.html', dados)
+    return render(request, 'contatos/contatos.html', dados)
+
+
+def contato(request, id):
+    contato = get_object_or_404(Contato, id=id)
+
+    dados = {
+        'contato': contato
+    }
+
+    return render(request, 'contatos/contato.html', dados)
