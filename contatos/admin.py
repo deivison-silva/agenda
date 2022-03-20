ec29461d7a6155c7f962d71cdb8e5b913b3b46fe
Deivison Silva <deivisondamiao01@outlook.com>
2022-03-20 16:38:33 -0300

    customizes classc contato and categoria on admin panel

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Categoria, Contato
 
 # Register your models here.
+
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'telefone', 'email',
+                    'data_criacao', 'categoria',)
+    list_display_links = ('id', 'nome',)
+    search_fields = ('nome',)
+    list_filter = ('categoria',)
+    list_editable = ('categoria',)
+    list_per_page = 10
+
+
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome',)
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+
+
+admin.site.register(Contato, ContatoAdmin)
+admin.site.register(Categoria, CategoriaAdmin)
