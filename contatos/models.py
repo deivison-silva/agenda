bbd7662a006e9d31e7c3c7a28d0240afe9d32647
Deivison Silva <deivisondamiao01@outlook.com>
2022-03-20 13:58:50 -0300

    adds models contato, do makemigrations and migrate to database

--- contatos/models.py
+++ contatos/models.py
@@ -1,3 +1,19 @@
+import email
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=100)
+    sobrenome = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, blank=True)
+    data_criacao = models.DateTimeField(default=timezone.now)
+    descricao = models.TextField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
