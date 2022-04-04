7f3963d28c108e046a7f9ba6519e5f22217d215d
Deivison Silva <deivisondamiao01@outlook.com>
2022-04-04 09:18:11 -0300

    changes debug config to false and allowed hosts

--- agenda/settings.py
+++ agenda/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-up-2=igk8lh)ba#d8ke6o#gfdmjn_b@rr@3w(ot1cn2gqv7rm('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['simple-agenda.herokuapp.com']
 
 
 # Application definition
